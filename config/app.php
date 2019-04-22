3fb8d7c0d811c94d03a0dc385fe1ed1483328954
Anton Tomin <iToha@iMac-Anton.local>
2019-04-22 14:20:59 +0300

    rollbar setup

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        Rollbar\Laravel\RollbarServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
